3ca7370a624d12ebb5cea4fb74597af386bbd583
BossanovaJJu <juk1126@gmail.com>
2020-04-01 15:26:39 +0900

    2th_file upload

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::where('is_completed', false)
+            ->orderBy('created_at', 'desc')
+            ->withCount(['tasks' => function ($query) {
+                $query->where('is_completed', false);
+            }])
+            ->get();
+        return $projects->toJson();
+
+    }
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+        $project = Project::create([
+            'name' => $validateData['name'],
+            'description' => $validateData['description'],
+        ]);
+
+        return response()->json('Project created');
+    }
+}
